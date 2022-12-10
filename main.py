f1cc9f3546aac57fd6c1c9ad8e1ae75ca7224799
Soundwave <PS-Soundwave@users.noreply.github.com>
2022-12-09 23:26:04 -0500

    Complete through day 8

--- main.py
+++ main.py
@@ -256,5 +256,169 @@ def day6_part2():
             return
 
 
+def reduce_fs(fs, acc):
+    if fs["type"] == "file":
+        return fs["size"], acc
+    size = 0
+    for obj in fs["contents"].keys():
+        osize, acc = reduce_fs(fs["contents"][obj], acc)
+        size += osize
+    if size <= 100000:
+        acc += size
+    return size, acc
+
+
+def reduce_fs2(fs, target, acc):
+    if fs["type"] == "file":
+        return fs["size"], acc
+    size = 0
+    for obj in fs["contents"].keys():
+        osize, acc = reduce_fs2(fs["contents"][obj], target, acc)
+        size += osize
+    if target <= size < acc:
+        acc = size
+    return size, acc
+
+
+def day7_part1():
+    lines = [line[:-1] for line in open("day7.txt").readlines()]
+
+    cd = []
+    fs = {"type": "dir", "contents": {}}
+
+    for line in lines:
+        if line == "$ ls":
+            continue
+        elif line == "$ cd /":
+            cd = []
+        elif line.startswith("$ cd"):
+            target = line.split(" ")[2]
+            if target == "..":
+                cd = cd[:-1]
+            else:
+                cd.append(target)
+        else:
+            cdo = fs
+            for d in cd:
+                cdo = cdo["contents"][d]
+            if line.startswith("dir"):
+                line = line.split(" ")
+                cdo["contents"][line[1]] = {"type": "dir", "contents": {}}
+            else:
+                line = line.split(" ")
+                cdo["contents"][line[1]] = {"type": "file", "size": int(line[0])}
+
+    print(reduce_fs(fs, 0)[1])
+
+
+def day7_part2():
+    lines = [line[:-1] for line in open("day7.txt").readlines()]
+
+    cd = []
+    fs = {"type": "dir", "contents": {}}
+
+    for line in lines:
+        if line == "$ ls":
+            continue
+        elif line == "$ cd /":
+            cd = []
+        elif line.startswith("$ cd"):
+            target = line.split(" ")[2]
+            if target == "..":
+                cd = cd[:-1]
+            else:
+                cd.append(target)
+        else:
+            cdo = fs
+            for d in cd:
+                cdo = cdo["contents"][d]
+            if line.startswith("dir"):
+                line = line.split(" ")
+                cdo["contents"][line[1]] = {"type": "dir", "contents": {}}
+            else:
+                line = line.split(" ")
+                cdo["contents"][line[1]] = {"type": "file", "size": int(line[0])}
+
+    size = reduce_fs(fs, 0)[0]
+
+    print(reduce_fs2(fs, size - 40000000, 30000000)[1])
+
+
+def day8_part1():
+    lines = [line[:-1] for line in open("day8.txt").readlines()]
+
+    height = [[int(tree) for tree in line] for line in lines]
+    visible = [[False for _ in line] for line in lines]
+
+    for i in range(0, len(height)):
+        h = -1
+        for j in range(0, len(height[0])):
+            if height[i][j] > h:
+                h = height[i][j]
+                visible[i][j] = True
+        h = -1
+        for j in range(len(height[0]) - 1, -1, -1):
+            if height[i][j] > h:
+                h = height[i][j]
+                visible[i][j] = True
+    for j in range(0, len(height[0])):
+        h = -1
+        for i in range(0, len(height)):
+            if height[i][j] > h:
+                h = height[i][j]
+                visible[i][j] = True
+        h = -1
+        for i in range(len(height) - 1, -1, -1):
+            if height[i][j] > h:
+                h = height[i][j]
+                visible[i][j] = True
+
+    count = 0
+    for i in range(0, len(height)):
+        for j in range(0, len(height[0])):
+            if visible[i][j]:
+                count += 1
+
+    print(count)
+
+
+def day8_part2():
+    lines = [line[:-1] for line in open("day8.txt").readlines()]
+
+    height = [[int(tree) for tree in line] for line in lines]
+    result = 0
+
+    for i in range(0, len(height)):
+        for j in range(0, len(height[0])):
+            up = 0
+            down = 0
+            left = 0
+            right = 0
+            for k in range(j - 1, -1, -1):
+                if height[i][k] <= height[i][j]:
+                    up += 1
+                if height[i][k] >= height[i][j]:
+                    break
+            for k in range(j + 1, len(height[0])):
+                if height[i][k] <= height[i][j]:
+                    down += 1
+                if height[i][k] >= height[i][j]:
+                    break
+            for k in range(i - 1, -1, -1):
+                if height[k][j] <= height[i][j]:
+                    left += 1
+                if height[k][j] >= height[i][j]:
+                    break
+            for k in range(i + 1, len(height)):
+                if height[k][j] <= height[i][j]:
+                    right += 1
+                if height[k][j] >= height[i][j]:
+                    break
+
+            result = max(result, up * down * left * right)
+
+    print(result)
+
+
 if __name__ == '__main__':
-    day4_part2()
+    day8_part2()
